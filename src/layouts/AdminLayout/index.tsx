cc11fa9842b5c8fec0059cd3b93ecc493d1a7235
Moreka_Oleksandr <73757965+S4nya228@users.noreply.github.com>
2025-03-31 19:56:51 +0300

    ui: add admin page

--- src/layouts/AdminLayout/index.tsx
+++ src/layouts/AdminLayout/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from '../../pages/NotFound'
+import AdminPanel from '../../pages/AdminPanel'
 
 function AdminLayout() {
 	return (
 		<Routes>
+			<Route path="/1" element={<AdminPanel />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	)
